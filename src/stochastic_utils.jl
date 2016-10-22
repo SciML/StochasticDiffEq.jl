f05ae69c5b3c5b78b2879f28fc74f7765f0ecab0
ChrisRackauckas <me@chrisrackauckas.com>
2016-10-22 09:39:44 -0700

    Remove finite element util

--- src/stochastic_utils.jl
+++ src/stochastic_utils.jl
@@ -1,15 +1,4 @@
 """
-`getNoise(N,node,elem;noisetype=:White)`
-
-Returns a random vector corresponding to the noise type which was chosen.
-"""
-function getNoise(u,node,elem;noisetype=:White)
-  if noisetype==:White
-    return(ChunkedArray(randn,u))
-  end
-end
-
-"""
 `monteCarloSim(Δt::Number,prob::SDEProblem)`
 
 Performs a parallel Monte-Carlo simulation to solve the SDE problem with Δt numMonte times.
