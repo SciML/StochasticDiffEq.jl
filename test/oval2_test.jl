a0b62a7b672c33115a4e588f358af2070264434a
ChrisRackauckas <me@chrisrackauckas.com>
2016-11-01 00:52:30 -0700

    change to diffeqproblem library

--- test/oval2_test.jl
+++ test/oval2_test.jl
@@ -2,8 +2,8 @@
 
 prob_sde_lorenz
 
-StochasticDiffEq.oval2ModelExample()
+DiffEqProblemLibrary.oval2ModelExample()
 
-StochasticDiffEq.oval2ModelExample(useBigs=true,largeFluctuations=true)
+DiffEqProblemLibrary.oval2ModelExample(useBigs=true,largeFluctuations=true)
 
 true
