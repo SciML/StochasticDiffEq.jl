1de9fd9ea939201057f2e6f0c2d9b1b5358174e9
ChrisRackauckas <me@chrisrackauckas.com>
2017-01-24 23:25:17 -0800

    perform_step on low order

--- src/integrators/low_order.jl
+++ src/integrators/low_order.jl
@@ -1,60 +1,76 @@
+@inline function perform_step!(integrator,cache::EMConstantCache,f=integrator.f)
+  @unpack t,dt,uprev,u,ΔW = integrator
+  u = uprev + dt.*integrator.f(t,uprev) + integrator.g(t,uprev).*ΔW
+  @pack integrator = t,dt,u
+end
+
 function sde_solve{uType<:Number,uEltype,Nm1,N,tType,tTypeNoUnits,uEltypeNoUnits,randType,randElType,rateType,solType,cacheType,progType,Stack1Type,Stack2Type,F4,F5,OType}(integrator::SDEIntegrator{EM,uType,uEltype,Nm1,N,tType,tTypeNoUnits,uEltypeNoUnits,randType,randElType,rateType,solType,cacheType,progType,Stack1Type,Stack2Type,F4,F5,OType})
   @inbounds while integrator.t < integrator.T
     @sde_loopheader
-    @unpack t,dt,uprev,u,ΔW = integrator
-    u = uprev + dt.*integrator.f(t,uprev) + integrator.g(t,uprev).*ΔW
-    @pack integrator = t,dt,u
+    perform_step!(integrator,integrator.cache)
     loopfooter!(integrator)
   end
   @sde_postamble
+end
+
+@inline function perform_step!(integrator,cache::EMCache,f=integrator.f)
+  @unpack utmp1,utmp2 = integrator.cache
+  @unpack t,dt,uprev,u,ΔW = integrator
+  integrator.f(t,uprev,utmp1)
+  integrator.g(t,uprev,utmp2)
+  for i in eachindex(u)
+    u[i] = uprev[i] + dt*utmp1[i] + utmp2[i]*ΔW[i]
+  end
+  @pack integrator = t,dt,u
 end
 
 function sde_solve{uType<:AbstractArray,uEltype,Nm1,N,tType,tTypeNoUnits,uEltypeNoUnits,randType,randElType,rateType,solType,cacheType,progType,Stack1Type,Stack2Type,F4,F5,OType}(integrator::SDEIntegrator{EM,uType,uEltype,Nm1,N,tType,tTypeNoUnits,uEltypeNoUnits,randType,randElType,rateType,solType,cacheType,progType,Stack1Type,Stack2Type,F4,F5,OType})
   @inbounds while integrator.t < integrator.T
     @sde_loopheader
-    @unpack utmp1,utmp2 = integrator.cache
-    @unpack t,dt,uprev,u,ΔW = integrator
-    integrator.f(t,uprev,utmp1)
-    integrator.g(t,uprev,utmp2)
-    for i in eachindex(u)
-      u[i] = uprev[i] + dt*utmp1[i] + utmp2[i]*ΔW[i]
-    end
-    @pack integrator = t,dt,u
+    perform_step!(integrator,integrator.cache)
     loopfooter!(integrator)
   end
   @sde_postamble
+end
+
+@inline function perform_step!(integrator,cache::RKMilCache,f=integrator.f)
+  @unpack du1,du2,K,utilde,L = integrator.cache
+  @unpack t,dt,uprev,u,ΔW = integrator
+  integrator.f(t,uprev,du1)
+  integrator.g(t,uprev,L)
+  for i in eachindex(u)
+    K[i] = uprev[i] + dt*du1[i]
+    utilde[i] = K[i] + L[i]*integrator.sqdt
+  end
+  integrator.g(t,utilde,du2)
+  for i in eachindex(u)
+    u[i] = K[i]+L[i]*ΔW[i]+(du2[i]-L[i])./(2integrator.sqdt).*(ΔW[i].^2 - dt)
+  end
+  @pack integrator = t,dt,u
 end
 
 function sde_solve{uType<:AbstractArray,uEltype,Nm1,N,tType,tTypeNoUnits,uEltypeNoUnits,randType,randElType,rateType,solType,cacheType,progType,Stack1Type,Stack2Type,F4,F5,OType}(integrator::SDEIntegrator{RKMil,uType,uEltype,Nm1,N,tType,tTypeNoUnits,uEltypeNoUnits,randType,randElType,rateType,solType,cacheType,progType,Stack1Type,Stack2Type,F4,F5,OType})
   @inbounds while integrator.t < integrator.T
     @sde_loopheader
-    @unpack du1,du2,K,utilde,L = integrator.cache
-    @unpack t,dt,uprev,u,ΔW = integrator
-    integrator.f(t,uprev,du1)
-    integrator.g(t,uprev,L)
-    for i in eachindex(u)
-      K[i] = uprev[i] + dt*du1[i]
-      utilde[i] = K[i] + L[i]*integrator.sqdt
-    end
-    integrator.g(t,utilde,du2)
-    for i in eachindex(u)
-      u[i] = K[i]+L[i]*ΔW[i]+(du2[i]-L[i])./(2integrator.sqdt).*(ΔW[i].^2 - dt)
-    end
-    @pack integrator = t,dt,u
+    perform_step!(integrator,integrator.cache)
     loopfooter!(integrator)
   end
   @sde_postamble
 end
 
+@inline function perform_step!(integrator,cache::RKMilConstantCache,f=integrator.f)
+  @unpack t,dt,uprev,u,ΔW = integrator
+  K = uprev + dt.*integrator.f(t,uprev)
+  L = integrator.g(t,uprev)
+  utilde = K + L*integrator.sqdt
+  u = K+L*ΔW+(integrator.g(t,utilde)-integrator.g(t,uprev))/(2integrator.sqdt)*(ΔW^2 - dt)
+  @pack integrator = t,dt,u
+end
+
 function sde_solve{uType<:Number,uEltype,Nm1,N,tType,tTypeNoUnits,uEltypeNoUnits,randType,randElType,rateType,solType,cacheType,progType,Stack1Type,Stack2Type,F4,F5,OType}(integrator::SDEIntegrator{RKMil,uType,uEltype,Nm1,N,tType,tTypeNoUnits,uEltypeNoUnits,randType,randElType,rateType,solType,cacheType,progType,Stack1Type,Stack2Type,F4,F5,OType})
   @inbounds while integrator.t < integrator.T
     @sde_loopheader
-    @unpack t,dt,uprev,u,ΔW = integrator
-    K = uprev + dt.*integrator.f(t,uprev)
-    L = integrator.g(t,uprev)
-    utilde = K + L*integrator.sqdt
-    u = K+L*ΔW+(integrator.g(t,utilde)-integrator.g(t,uprev))/(2integrator.sqdt)*(ΔW^2 - dt)
-    @pack integrator = t,dt,u
+    perform_step!(integrator,integrator.cache)
     loopfooter!(integrator)
   end
   @sde_postamble
