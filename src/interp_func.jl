a79704106157341fa9006a0c569058a6637f25f0
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-06-05 12:31:57 -0700

    interp display

--- src/interp_func.jl
+++ src/interp_func.jl
@@ -3,5 +3,6 @@ immutable LinearInterpolationData{uType,tType} <: Function
   ts::tType
 end
 
+DiffEqBase.interp_summary(::LinearInterpolationData) = "First Order Linear"
 (interp::LinearInterpolationData)(tvals,idxs,deriv) = sde_interpolation(tvals,interp,idxs,deriv)
 (interp::LinearInterpolationData)(val,tvals,idxs,deriv) = sde_interpolation!(val,tvals,interp,idxs,deriv)
