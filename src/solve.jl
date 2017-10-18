561073b6b36198f09a4a7f72f86fe362d1f2c4e8
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-10-18 09:58:03 -0700

    initialize checking for individual modifications

--- src/solve.jl
+++ src/solve.jl
@@ -391,13 +391,12 @@ function init(
   DiffEqNoiseProcess.setup_next_step!(integrator.W)
 
   if initialize_integrator
-    integrator.u_modified = true
 
-    initialize!(callbacks_internal,t,u,integrator)
+    u_modified = initialize!(callbacks_internal,t,u,integrator)
 
     # if the user modifies u, we need to fix previous values before initializing
     # FSAL in order for the starting derivatives to be correct
-    if integrator.u_modified
+    if u_modified
       if alg_extrapolates(integrator.alg)
         if isinplace(integrator.sol.prob)
           recursivecopy!(integrator.uprev2,integrator.uprev)
@@ -410,7 +409,13 @@ function init(
       else
         integrator.uprev = integrator.u
       end
+
+      # reset this as it is now handled so the integrators should proceed as normal
       integrator.u_modified = false
+
+      if initialize_save && any((c)->c.save_positions[2],callbacks_internal)
+        savevalues!(integrator,true)
+      end
     end
 
     initialize!(integrator,integrator.cache)
