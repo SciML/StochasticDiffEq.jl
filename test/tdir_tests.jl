f53d32cbfba78920cbc2ad610f51a128cc68df66
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-10-20 12:07:55 -0700

    don't use problem mutation

--- test/tdir_tests.jl
+++ test/tdir_tests.jl
@@ -1,23 +1,23 @@
 using StochasticDiffEq, DiffEqDevTools, DiffEqBase, DiffEqProblemLibrary, Base.Test
 srand(100)
 prob = deepcopy(prob_sde_linear)
-prob.tspan = (1.0,0.0)
+prob2 = SDEProblem(prob.f,prob.g,prob.u0,(1.0,0.0))
 
-sol = solve(prob,EM(),dt=-1//2^(4),tstops = [0.33])
+sol = solve(prob2,EM(),dt=-1//2^(4),tstops = [0.33])
 
 @test sol.t[15] > sol.t[16]
 
-sol = solve(prob,SRIW1(),tstops = [0.33])
+sol = solve(prob2,SRIW1(),tstops = [0.33])
 
 @test sol.t[15] > sol.t[16]
 
 prob = deepcopy(prob_sde_2Dlinear)
-prob.tspan = (1.0,0.0)
+prob2 = SDEProblem(prob.f,prob.g,prob.u0,(1.0,0.0))
 
-sol = solve(prob,EM(),dt=-1//2^(4),tstops = [0.33])
+sol = solve(prob2,EM(),dt=-1//2^(4),tstops = [0.33])
 
 @test sol.t[15] > sol.t[16]
 
-sol = solve(prob,SRIW1(),tstops = [0.33])
+sol = solve(prob2,SRIW1(),tstops = [0.33])
 
 @test sol.t[15] > sol.t[16]
