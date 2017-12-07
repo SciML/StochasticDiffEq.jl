29ffbc1ec69e99ad503d5903dd945e7e1081ff3b
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-12-06 23:36:19 -0800

    uncomment static array test

--- test/static_array_tests.jl
+++ test/static_array_tests.jl
@@ -13,7 +13,7 @@ u0 = zeros(SVector{2,Float64}, 2) + 1
 u0[1] = ones(SVector{2,Float64}) + 1
 ode = SDEProblem(f, f, u0, (0.,1.))
 sol = solve(ode, EM(), dt=1.e-2)
-#sol = solve(ode, SRIW1(), dt=1.e-2)
+sol = solve(ode, SRIW1(), dt=1.e-2)
 
 
 u0 = ones(MVector{2,Float64})
