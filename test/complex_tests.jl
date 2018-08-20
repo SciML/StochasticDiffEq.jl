9c08a5e3992234fafcbe1aacacd87bed786a95f9
YingboMa <mayingbo5@gmail.com>
2018-08-20 17:28:21 -0400

    Complex compatibility for implicit non-autodiff algs

--- test/complex_tests.jl
+++ test/complex_tests.jl
@@ -1,16 +1,23 @@
-using DiffEqBase, StochasticDiffEq
-
-f(u,p,t)=1.01u
-g(u,p,t)=1.01u
+using DiffEqBase, StochasticDiffEq, Test
+explicit = [SRIW1()]
+implicit_autodiff   = [SKenCarp(), ImplicitEulerHeun()]
+implicit_noautodiff = [SKenCarp(autodiff=false), ImplicitEulerHeun(autodiff=false)]
+oopf(u,p,t)=1.01u
+oopg(u,p,t)=1.01u
 u0 = 1.0+1.0im
 tspan = (0.0,1.0)
-prob = SDEProblem(f,g,u0,tspan)
-sol = solve(prob,SRIW1())
-typeof(sol[end]) == ComplexF64
-
-f(du,u,p,t) = (du.=1.01u)
-g(du,u,p,t) = (du.=1.01u)
+prob_oop = SDEProblem(oopf,oopg,u0,tspan)
+iipf(du,u,p,t) = (du.=1.01u)
+iipg(du,u,p,t) = (du.=1.01u)
 u0 = ones(2,4) + im*ones(2,4)
-prob = SDEProblem(f,g,u0,tspan)
-sol = solve(prob,SRIW1())
-eltype(sol[end]) == ComplexF64
+prob_iip = SDEProblem(iipf,iipg,u0,tspan)
+
+for alg in vcat(explicit, implicit_noautodiff), prob in [prob_iip, prob_oop]
+  sol = solve(prob,alg)
+  @test eltype(sol[end]) == ComplexF64
+end
+
+for alg in implicit_autodiff, prob in [prob_iip, prob_oop]
+  @test_broken sol = solve(prob,alg)
+  #eltype(sol[end]) == ComplexF64
+end
