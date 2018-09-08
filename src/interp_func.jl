4253d3394eaefaaf007ef84c096badf8f9f3e52a
YingboMa <mayingbo5@gmail.com>
2018-09-07 22:15:55 -0400

    Add more stub methods

--- src/interp_func.jl
+++ src/interp_func.jl
@@ -4,5 +4,5 @@ struct LinearInterpolationData{uType,tType} <: AbstractDiffEqInterpolation
 end
 
 DiffEqBase.interp_summary(::LinearInterpolationData) = "1st order linear"
-(interp::LinearInterpolationData)(tvals,idxs,deriv,p,continuity) = sde_interpolation(tvals,interp,idxs,deriv,p,continuity)
-(interp::LinearInterpolationData)(val,tvals,idxs,deriv,p,continuity) = sde_interpolation!(val,tvals,interp,idxs,deriv,p,continuity)
+(interp::LinearInterpolationData)(tvals,idxs,deriv,p,continuity::Symbol=:left) = sde_interpolation(tvals,interp,idxs,deriv,p,continuity)
+(interp::LinearInterpolationData)(val,tvals,idxs,deriv,p,continuity::Symbol=:left) = sde_interpolation!(val,tvals,interp,idxs,deriv,p,continuity)
