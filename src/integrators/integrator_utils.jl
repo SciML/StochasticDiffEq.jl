46fb80f3c4223c1e173f239d396d82cde1e5eafe
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-10-22 11:07:12 -0700

    remove export assumption

--- src/integrators/integrator_utils.jl
+++ src/integrators/integrator_utils.jl
@@ -217,16 +217,16 @@ end
   saved_in_cb = false
   if !(typeof(continuous_callbacks)<:Tuple{})
     time,upcrossing,event_occurred,idx,counter =
-              find_first_continuous_callback(integrator,continuous_callbacks...)
+              DiffEqBase.find_first_continuous_callback(integrator,continuous_callbacks...)
     if event_occurred
       integrator.event_last_time = idx
-      continuous_modified,saved_in_cb = apply_callback!(integrator,continuous_callbacks[idx],time,upcrossing)
+      continuous_modified,saved_in_cb = DiffEqBase.apply_callback!(integrator,continuous_callbacks[idx],time,upcrossing)
     else
       integrator.event_last_time = 0
     end
   end
   if !(typeof(discrete_callbacks)<:Tuple{})
-    discrete_modified,saved_in_cb = apply_discrete_callback!(integrator,discrete_callbacks...)
+    discrete_modified,saved_in_cb = DiffEqBase.apply_discrete_callback!(integrator,discrete_callbacks...)
   end
   if !saved_in_cb
     savevalues!(integrator)
