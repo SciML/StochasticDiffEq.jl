4ba525b569d899b44b9a9a0333429df1553579d2
Kanav Gupta <kanav0610@gmail.com>
2019-05-06 22:09:53 +0530

    Fixes SKenCarp convergence test

--- test/sde/sde_convergence_tests.jl
+++ test/sde/sde_convergence_tests.jl
@@ -99,7 +99,7 @@ sim9 = test_convergence(dts,prob,SOSRA2(),numMonte=Int(1e1))
 @test abs(sim9.𝒪est[:final]-2) < 0.3
 dts = (1/2) .^ (10:-1:4) #14->7 good plot
 sim10 = test_convergence(dts,prob,SKenCarp(),numMonte=Int(1e1))
-@test abs(sim10.𝒪est[:final]-2) < 0.3
+@test abs(sim10.𝒪est[:final]-1.5) < 0.3
 sim10 = test_convergence(dts,prob,SKenCarp(nlsolve=StochasticDiffEq.NLFunctional()),numMonte=Int(1e1))
 @test abs(sim10.𝒪est[:final]-2) < 0.3
 sim11 = test_convergence(dts,prob,SKenCarp(nlsolve=StochasticDiffEq.NLNewton()),numMonte=Int(1e1))
