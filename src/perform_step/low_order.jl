9506a00fb4b766a7164b24abc615a05d84ed62bd
deeepeshthakur <deeepeshthakur@gmail.com>
2019-05-31 12:25:59 +0530

    fix bugs in Wangli Methods

--- src/perform_step/low_order.jl
+++ src/perform_step/low_order.jl
@@ -243,14 +243,14 @@ end
   tmp = u + k*integrator.sqdt
   k₁ = integrator.g(tmp,p,t)
   k₁ = (k₁ - k)/(integrator.sqdt)
-  u  = u - (dt/2)*k*k₁
+  u  = u - (dt/2)*k₁
 
   #stage 3
   k  = integrator.g(u,p,t)
   tmp = u + k*integrator.sqdt
   k₁ = integrator.g(tmp,p,t)
   k₁ = (k₁ - k)/(integrator.sqdt)
-  u  = u + k*W.dW + (W.dW^2/2)*k*k₁
+  u  = u + k*W.dW + (W.dW^2/2)*k₁
   integrator.u = u
 end
 
@@ -267,14 +267,14 @@ end
   @.. tmp = u + k*integrator.sqdt
   integrator.g(k₁,tmp,p,t)
   @.. k₁ = (k₁ - k)/(integrator.sqdt)
-  @.. u  = u - (dt/2)*k*k₁
+  @.. u  = u - (dt/2)*k₁
 
   #stage 3
   integrator.g(k,u,p,t)
   @.. tmp = u + k*integrator.sqdt
   integrator.g(k₁,tmp,p,t)
   @.. k₁ = (k₁ - k)/(integrator.sqdt)
-  @.. u  = u + k*W.dW + (W.dW^2/2)*k*k₁
+  @.. u  = u + k*W.dW + (W.dW^2/2)*k₁
   integrator.u = u
 end
 
@@ -285,7 +285,7 @@ end
   tmp = uprev + k*integrator.sqdt
   k₁  = integrator.g(tmp,p,t)
   k₁  = (k₁ - k)/integrator.sqdt
-  u   = uprev + W.dW*k + (W.dW^2/2)*k*k₁
+  u   = uprev + W.dW*k + (W.dW^2/2)*k₁
 
   #stage 2
   k  = integrator.f(u,p,t)
@@ -296,7 +296,7 @@ end
   tmp = u + k*integrator.sqdt
   k₁  = integrator.g(tmp,p,t)
   k₁  = (k₁ - k)/integrator.sqdt
-  u   = u - (dt/2)*k*k₁
+  u   = u - (dt/2)*k₁
   integrator.u = u
 end
 
@@ -309,7 +309,7 @@ end
   @.. tmp = uprev + k*integrator.sqdt
   integrator.g(k₁,tmp,p,t)
   @.. k₁  = (k₁ - k)/integrator.sqdt
-  @.. u   = uprev + W.dW*k + (W.dW^2/2)*k*k₁
+  @.. u   = uprev + W.dW*k + (W.dW^2/2)*k₁
 
   #stage 2
   integrator.f(k,u,p,t)
@@ -320,7 +320,7 @@ end
   @.. tmp = u + k*integrator.sqdt
   integrator.g(k₁,tmp,p,t)
   @.. k₁  = (k₁ - k)/integrator.sqdt
-  @.. u   = u - (dt/2)*k*k₁
+  @.. u   = u - (dt/2)*k₁
 
   integrator.u = u
 end
