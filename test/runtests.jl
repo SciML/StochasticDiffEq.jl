6680261c8465dd280edd727cdadbc1b2a2860205
deeepeshthakur <deeepeshthakur@gmail.com>
2019-06-01 11:51:05 +0530

    rearranging tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,7 +19,6 @@ is_APPVEYOR = ( Sys.iswindows() && haskey(ENV,"APPVEYOR") )
     @time @testset "First Rand Tests" begin include("first_rand_test.jl") end
     @time @testset "Linear SDE Tests" begin include("sde/sde_linear_tests.jl") end
     @time @testset "Linear RODE Tests" begin include("rode_linear_tests.jl") end
-    @time @testset "Two-dimensional Linear SDE Tests" begin include("sde/sde_twodimlinear_tests.jl") end
     @time @testset "Number Type Tests" begin include("sde/sde_numbertype_tests.jl") end
     @time @testset "Complex Number Tests" begin include("complex_tests.jl") end
     @time @testset "Static Array Tests" begin include("static_array_tests.jl") end
@@ -37,6 +36,7 @@ is_APPVEYOR = ( Sys.iswindows() && haskey(ENV,"APPVEYOR") )
   end
 
   if group == "All" || group == "Interface2"
+    @time @testset "Two-dimensional Linear SDE Tests" begin include("sde/sde_twodimlinear_tests.jl") end
     @time @testset "Element-wise Tolerances Tests" begin include("tolerances_tests.jl") end
     @time @testset "Zero'd Noise Tests" begin include("zerod_noise_test.jl") end
     @time @testset "Scalar Tests" begin include("scalar_noise.jl") end
@@ -50,13 +50,13 @@ is_APPVEYOR = ( Sys.iswindows() && haskey(ENV,"APPVEYOR") )
   end
 
   if !is_APPVEYOR && (group == "All" || group == "AlgConvergence")
-    @time @testset "Additive SDE Tests" begin include("sde/sde_additive_tests.jl") end
     @time @testset "Non-diagonal SDE Tests" begin include("nondiagonal_tests.jl") end
     @time @testset "Rossler Order Tests" begin include("sde/sde_rosslerorder_tests.jl") end
     @time @testset "Convergence Tests" begin include("sde/sde_convergence_tests.jl") end
   end
 
   if !is_APPVEYOR && (group == "All" || group == "AlgConvergence2")
+    @time @testset "Additive SDE Tests" begin include("sde/sde_additive_tests.jl") end
     @time @testset "Split Tests" begin include("split_tests.jl") end
     @time @testset "Stratonovich Convergence Tests" begin include("stratonovich_convergence_tests.jl") end
     @time @testset "IIF Convergence Tests" begin include("iif_methods.jl") end
