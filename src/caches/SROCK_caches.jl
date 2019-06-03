9fc1de6d9c4e3b60369ecc916c9839179372eaa1
deeepeshthakur <deeepeshthakur@gmail.com>
2019-06-03 15:54:02 +0530

    perform_step

--- src/caches/SROCK_caches.jl
+++ src/caches/SROCK_caches.jl
@@ -1,13 +1,19 @@
-struct SROCK_1ConstantCache <: StochasticDiffEqConstantCache end
+mutable struct SROCK_1ConstantCache{zType} <: StochasticDiffEqConstantCache
+  zprev::zType
+end
 @cache struct SROCK_1Cache{uType,rateType} <: StochasticDiffEqMutableCache
   u::uType
   uprev::uType
   k::rateType
   k₁::rateType
   tmp::uType
+  fsalfirst::rateType
+  constantcache::SROCK_1ConstantCache
 end
 
-alg_cache(alg::SROCK_1,prob,u,ΔW,ΔZ,p,rate_prototype,noise_rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,f,t,dt,::Type{Val{false}}) = SROCK_1ConstantCache()
+function alg_cache(alg::SROCK_1,prob,u,ΔW,ΔZ,p,rate_prototype,noise_rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,f,t,dt,::Type{Val{false}})
+  SROCK_1ConstantCache(u)
+end
 
 function alg_cache(alg::SROCK_1,prob,u,ΔW,ΔZ,p,rate_prototype,noise_rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,f,t,dt,::Type{Val{true}})
   k = zero(rate_prototype)
--- src/perform_step/SROCK_perform_step.jl
+++ src/perform_step/SROCK_perform_step.jl
@@ -1,45 +1,107 @@
-@muladd function perform_step!(integrator,cache::WangLi3SMil_AConstantCache,f=integrator.f)
+@muladd function perform_step!(integrator,cache::SROCK_1ConstantCache,f=integrator.f)
   @unpack t,dt,uprev,u,W,p = integrator
+
+  maxeig!(integrator, cache)
+  mdeg = Int(floor(sqrt(2*dt*integrator.eigen_est/0.21)+1))
+  cache.mdeg = max(3,min(mdeg,250))
+  choose_deg!(integrator,cache)
+
+  η  = cache.optimal_η
+  ω₀ = 1 + η/(mdeg^2)
+  ωSq = ω₀^2 - 1
+  Sqrt_ω = sqrt(ωSq)
+  cosh_inv = log(ω₀ + Sqrt_ω)             # arcosh(ω₀)
+  ω₁ = (sinh(mdeg*cosh_inv)*ωSq)/(cosh(mdeg*cosh_inv)*mdeg*Sqrt_ω - ω₀*sinh(mdeg*cosh_inv))
+
+  α  = cosh(mdeg*cosh_inv)/(2*cosh((mdeg-1)*cosh_inv))
+  γ  = 1/(2*α)
+  β  = -γ
+
+  uᵢ₋₂ = copy(uprev)
+  fᵢ₋₁ = integrator.f(uprev,p,t)
+  Tᵢ₋₂ = one(eltype(u))
+  Tᵢ₋₁ = ω₀
+  Tᵢ   = Tᵢ₋₁
+  tᵢ₋₁ = t + dt*(ω₁/ω₀)
+  tᵢ   = tᵢ₋₁
+  tᵢ₋₂ = t
+
   #stage 1
-  k  = integrator.f(uprev,p,t)
-  u  = uprev + dt*k
+  uᵢ₋₁ = uprev + (dt*ω₁/ω₀)*fᵢ₋₁
 
-  #stage 2
-  k  = integrator.g(u,p,t)
-  tmp = u + k*integrator.sqdt
-  k₁ = integrator.g(tmp,p,t)
-  k₁ = (k₁ - k)/(integrator.sqdt)
-  u  = u - (dt/2)*k₁
-
-  #stage 3
-  k  = integrator.g(u,p,t)
-  tmp = u + k*integrator.sqdt
-  k₁ = integrator.g(tmp,p,t)
-  k₁ = (k₁ - k)/(integrator.sqdt)
-  u  = u + k*W.dW + (W.dW^2/2)*k₁
+  for i in 2:(mdeg)
+    Tᵢ = 2*ω₀*Tᵢ₋₁ - Tᵢ₋₂
+    μ   = 2*ω₁*Tᵢ₋₁/Tᵢ
+    ν   = 2*ω₀*Tᵢ₋₁/Tᵢ
+    κ = - Tᵢ₋₂/Tᵢ
+    fᵢ₋₁ = integrator.f(uᵢ₋₁,p,tᵢ₋₁)
+    u = dt*μ*fᵢ₋₁ + ν*uᵢ₋₁ + κ*uᵢ₋₂
+    (i == mdeg - 1) && (gₘ₋₂ = integrator.g(uᵢ₋₁,p,tᵢ₋₁); u = α*W.dW*gₘ₋₂)
+    (i == mdeg) && (gₘ₋₁ = integrator.g(uᵢ₋₁,p,tᵢ₋₁); u = β*W.dW*gₘ₋₂ + γ*W.dW*gₘ₋₁)
+    if i < mdeg
+      tᵢ = dt*μ + ν*tᵢ₋₁ + κ*tᵢ₋₂
+      uᵢ₋₂ = uᵢ₋₁
+      uᵢ₋₁ = u_i
+      tᵢ₋₂ = tᵢ₋₁
+      tᵢ₋₁ = tᵢ
+      Tᵢ₋₂ = Tᵢ₋₁
+      Tᵢ₋₁ = Tᵢ
+    end
+  end
   integrator.u = u
 end
 
-@muladd function perform_step!(integrator,cache::WangLi3SMil_ACache,f=integrator.f)
-  @unpack tmp,k,k₁ = cache
+@muladd function perform_step!(integrator,cache::SROCK_1Cache,f=integrator.f)
+  @unpack uᵢ₋₁,fᵢ₋₁, gₘ₋₁, gₘ₋₂ = cache
   @unpack t,dt,uprev,u,W,p = integrator
+  ccache = cache.constantcache
+  maxeig!(integrator, cache)
+  mdeg = Int(floor(sqrt(2*dt*integrator.eigen_est/0.21)+1))
+  ccache.mdeg = max(3,min(mdeg,250))
+  choose_deg!(integrator,cache)
+  uᵢ₋₂ = cache.tmp
+
+  η  = ccache.optimal_η
+  ω₀ = 1 + η/(mdeg^2)
+  ωSq = ω₀^2 - 1
+  Sqrt_ω = sqrt(ωSq)
+  cosh_inv = log(ω₀ + Sqrt_ω)             # arcosh(ω₀)
+  ω₁ = (sinh(mdeg*cosh_inv)*ωSq)/(cosh(mdeg*cosh_inv)*mdeg*Sqrt_ω - ω₀*sinh(mdeg*cosh_inv))
+
+  α  = cosh(mdeg*cosh_inv)/(2*cosh((mdeg-1)*cosh_inv))
+  γ  = 1/(2*α)
+  β  = -γ
+
+  @.. uᵢ₋₂ = uprev
+  integrator.f(fᵢ₋₁,uprev,p,t)
+  Tᵢ₋₂ = one(eltype(u))
+  Tᵢ₋₁ = ω₀
+  Tᵢ   = Tᵢ₋₁
+  tᵢ₋₁ = t + dt*(ω₁/ω₀)
+  tᵢ   = tᵢ₋₁
+  tᵢ₋₂ = t
 
   #stage 1
-  integrator.f(k,uprev,p,t)
-  @.. u  = uprev + dt*k
+  @.. uᵢ₋₁ = uprev + (dt*ω₁/ω₀)*fᵢ₋₁
 
-  #stage 2
-  integrator.g(k,u,p,t)
-  @.. tmp = u + k*integrator.sqdt
-  integrator.g(k₁,tmp,p,t)
-  @.. k₁ = (k₁ - k)/(integrator.sqdt)
-  @.. u  = u - (dt/2)*k₁
-
-  #stage 3
-  integrator.g(k,u,p,t)
-  @.. tmp = u + k*integrator.sqdt
-  integrator.g(k₁,tmp,p,t)
-  @.. k₁ = (k₁ - k)/(integrator.sqdt)
-  @.. u  = u + k*W.dW + (W.dW^2/2)*k₁
+  for i in 2:(mdeg)
+    Tᵢ = 2*ω₀*Tᵢ₋₁ - Tᵢ₋₂
+    μ   = 2*ω₁*Tᵢ₋₁/Tᵢ
+    ν   = 2*ω₀*Tᵢ₋₁/Tᵢ
+    κ = - Tᵢ₋₂/Tᵢ
+    integrator.f(fᵢ₋₁,uᵢ₋₁,p,tᵢ₋₁)
+    @.. u = dt*μ*fᵢ₋₁ + ν*uᵢ₋₁ + κ*uᵢ₋₂
+    (i == mdeg - 1) && (integrator.g(gₘ₋₂,uᵢ₋₁,p,tᵢ₋₁); @.. u += α*W.dW*gₘ₋₂)
+    (i == mdeg) && (integrator.g(gₘ₋₁,uᵢ₋₁,p,tᵢ₋₁); @.. u = β*W.dW*gₘ₋₂ + γ*W.dW*gₘ₋₁)
+    if i < mdeg
+      tᵢ = dt*μ + ν*tᵢ₋₁ + κ*tᵢ₋₂
+      @.. uᵢ₋₂ = uᵢ₋₁
+      @.. uᵢ₋₁ = u_i
+      tᵢ₋₂ = tᵢ₋₁
+      tᵢ₋₁ = tᵢ
+      Tᵢ₋₂ = Tᵢ₋₁
+      Tᵢ₋₁ = Tᵢ
+    end
+  end
   integrator.u = u
 end
