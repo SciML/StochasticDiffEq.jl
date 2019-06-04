5c1932dae39380f3bd365b5b38e9da66950b8de6
deeepeshthakur <deeepeshthakur@gmail.com>
2019-06-04 13:26:33 +0530

    temp state

--- src/perform_step/SROCK_perform_step.jl
+++ src/perform_step/SROCK_perform_step.jl
@@ -2,18 +2,19 @@
   @unpack t,dt,uprev,u,W,p = integrator
 
   maxeig!(integrator, cache)
-  mdeg = Int(floor(sqrt(2*dt*integrator.eigen_est/0.21)+1))
-  cache.mdeg = max(3,min(mdeg,250))
+  mdeg = Int(floor(sqrt(2*dt*integrator.eigen_est/0.66)+1))
+  cache.mdeg = max(5,min(mdeg,250))
   choose_deg!(integrator,cache)
 
+  mdeg = cache.mdeg
   η  = cache.optimal_η
   ω₀ = 1 + η/(mdeg^2)
   ωSq = ω₀^2 - 1
   Sqrt_ω = sqrt(ωSq)
   cosh_inv = log(ω₀ + Sqrt_ω)             # arcosh(ω₀)
-  ω₁ = (sinh(mdeg*cosh_inv)*ωSq)/(cosh(mdeg*cosh_inv)*mdeg*Sqrt_ω - ω₀*sinh(mdeg*cosh_inv))
+  ω₁ = (Sqrt_ω*cosh(mdeg*cosh_inv))/(mdeg*sinh(mdeg*cosh_inv))
 
-  α  = cosh(mdeg*cosh_inv)/(2*cosh((mdeg-1)*cosh_inv))
+  α  = cosh(mdeg*cosh_inv)/(2*ω₀*cosh((mdeg-1)*cosh_inv))
   γ  = 1/(2*α)
   β  = -γ
 
@@ -25,23 +26,30 @@
   tᵢ₋₁ = t + dt*(ω₁/ω₀)
   tᵢ   = tᵢ₋₁
   tᵢ₋₂ = t
+  gₘ₋₁ = zero(k)
+  gₘ₋₂ = zero(k)
 
   #stage 1
   uᵢ₋₁ = uprev + (dt*ω₁/ω₀)*k
 
   for i in 2:mdeg
+    println(t,"   ",tᵢ)
     Tᵢ = 2*ω₀*Tᵢ₋₁ - Tᵢ₋₂
-    μ   = 2*ω₁*Tᵢ₋₁/Tᵢ
-    ν   = 2*ω₀*Tᵢ₋₁/Tᵢ
+    μ = 2*ω₁*Tᵢ₋₁/Tᵢ
+    ν = 2*ω₀*Tᵢ₋₁/Tᵢ
     κ = - Tᵢ₋₂/Tᵢ
-    k = integrator.f(uᵢ₋₁,p,tᵢ₋₁)
+    # k = integrator.f(uᵢ₋₁,p,tᵢ₋₁)
+    k = integrator.f(uᵢ₋₁,p,t)
     u = dt*μ*k + ν*uᵢ₋₁ + κ*uᵢ₋₂
-    (i == mdeg - 1) && (gₘ₋₂ = integrator.g(uᵢ₋₁,p,tᵢ₋₁); u = α*W.dW*gₘ₋₂)
-    (i == mdeg) && (gₘ₋₁ = integrator.g(uᵢ₋₁,p,tᵢ₋₁); u = β*W.dW*gₘ₋₂ + γ*W.dW*gₘ₋₁)
+    # (i == mdeg - 1) && (gₘ₋₂ = integrator.g(uᵢ₋₁,p,tᵢ₋₁); u += α*W.dW*gₘ₋₂)
+    (i == mdeg - 1) && (gₘ₋₂ = integrator.g(uᵢ₋₁,p,t); u += α*W.dW*gₘ₋₂)
+    # (i == mdeg) && (gₘ₋₁ = integrator.g(uᵢ₋₁,p,tᵢ₋₁); u += β*W.dW*gₘ₋₂ + γ*W.dW*gₘ₋₁)
+    (i == mdeg) && (gₘ₋₁ = integrator.g(uᵢ₋₁,p,t); u += β*W.dW*gₘ₋₂ + γ*W.dW*gₘ₋₁)
+
     if i < mdeg
       tᵢ = dt*μ + ν*tᵢ₋₁ + κ*tᵢ₋₂
       uᵢ₋₂ = uᵢ₋₁
-      uᵢ₋₁ = u_i
+      uᵢ₋₁ = u
       tᵢ₋₂ = tᵢ₋₁
       tᵢ₋₁ = tᵢ
       Tᵢ₋₂ = Tᵢ₋₁
@@ -57,16 +65,16 @@ end
   ccache = cache.constantcache
   maxeig!(integrator, cache)
   mdeg = Int(floor(sqrt(2*dt*integrator.eigen_est/0.21)+1))
-  ccache.mdeg = max(3,min(mdeg,250))
+  ccache.mdeg = max(5,min(mdeg,250))
   choose_deg!(integrator,cache)
   uᵢ₋₂ = cache.tmp
-
+  mdeg = ccache.mdeg
   η  = ccache.optimal_η
   ω₀ = 1 + η/(mdeg^2)
   ωSq = ω₀^2 - 1
   Sqrt_ω = sqrt(ωSq)
   cosh_inv = log(ω₀ + Sqrt_ω)             # arcosh(ω₀)
-  ω₁ = (sinh(mdeg*cosh_inv)*ωSq)/(cosh(mdeg*cosh_inv)*mdeg*Sqrt_ω - ω₀*sinh(mdeg*cosh_inv))
+  ω₁ = (Sqrt_ω*cosh(mdeg*cosh_inv))/(mdeg*sinh(mdeg*cosh_inv))
 
   α  = cosh(mdeg*cosh_inv)/(2*cosh((mdeg-1)*cosh_inv))
   γ  = 1/(2*α)
@@ -86,17 +94,17 @@ end
 
   for i in 2:mdeg
     Tᵢ = 2*ω₀*Tᵢ₋₁ - Tᵢ₋₂
-    μ   = 2*ω₁*Tᵢ₋₁/Tᵢ
-    ν   = 2*ω₀*Tᵢ₋₁/Tᵢ
+    μ = 2*ω₁*Tᵢ₋₁/Tᵢ
+    ν = 2*ω₀*Tᵢ₋₁/Tᵢ
     κ = - Tᵢ₋₂/Tᵢ
     integrator.f(k,uᵢ₋₁,p,tᵢ₋₁)
     @.. u = dt*μ*k + ν*uᵢ₋₁ + κ*uᵢ₋₂
     (i == mdeg - 1) && (integrator.g(gₘ₋₂,uᵢ₋₁,p,tᵢ₋₁); @.. u += α*W.dW*gₘ₋₂)
-    (i == mdeg) && (integrator.g(gₘ₋₁,uᵢ₋₁,p,tᵢ₋₁); @.. u = β*W.dW*gₘ₋₂ + γ*W.dW*gₘ₋₁)
+    (i == mdeg) && (integrator.g(gₘ₋₁,uᵢ₋₁,p,tᵢ₋₁); @.. u += β*W.dW*gₘ₋₂ + γ*W.dW*gₘ₋₁)
     if i < mdeg
       tᵢ = dt*μ + ν*tᵢ₋₁ + κ*tᵢ₋₂
       @.. uᵢ₋₂ = uᵢ₋₁
-      @.. uᵢ₋₁ = u_i
+      @.. uᵢ₋₁ = u
       tᵢ₋₂ = tᵢ₋₁
       tᵢ₋₁ = tᵢ
       Tᵢ₋₂ = Tᵢ₋₁
