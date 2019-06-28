4fe33449781f176717949fb59890e830972f1911
deeepeshthakur <deeepeshthakur@gmail.com>
2019-06-28 08:04:32 +0530

    added KomBurSROCK2

--- src/alg_utils.jl
+++ src/alg_utils.jl
@@ -39,6 +39,7 @@ alg_order(alg::RKMilCommute) = 1//1
 # diagonal noise or commutative noise
 alg_order(alg::SROCK1) = 1//2
 alg_order(alg::SROCK2) = 1//1
+alg_order(alg::KomBurSROCK2) = 1//1
 alg_order(alg::SROCKEM) = alg.strong_order_1 ? 1//1 : 1//2
 alg_order(alg::SKSROCK) = 1//2
 alg_order(alg::TangXiaoSROCK2) = 1//1
@@ -97,6 +98,7 @@ alg_compatible(prob,alg::WangLi3SMil_E) = true
 alg_compatible(prob,alg::WangLi3SMil_F) = true
 alg_compatible(prob,alg::SROCK1) = true
 alg_compatible(prob,alg::SROCK2) = true
+alg_compatible(prob,alg::KomBurSROCK2) = true
 alg_compatible(prob,alg::SROCKEM) = true
 alg_compatible(prob,alg::SKSROCK) = true
 alg_compatible(prob,alg::TangXiaoSROCK2) = true
