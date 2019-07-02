168cbf9117ce36a936484af87aa672ff676ad25d
deeepeshthakur <deeepeshthakur@gmail.com>
2019-07-02 13:31:16 +0530

    fix cache

--- src/caches/SROCK_caches.jl
+++ src/caches/SROCK_caches.jl
@@ -238,14 +238,16 @@ end
 @cache struct KomBurSROCK2Cache{uType,rateType,noiseRateType,T} <: StochasticDiffEqMutableCache
   u::uType
   uprev::uType
-  uᵢ::uType
+  utmp::uType
   uᵢ₋₁::uType
   uᵢ₋₂::uType
-  uₛ₋₁::uType
-  Yₛ₋₁::uType
-  Yₛ₋₂::uType
   k::rateType
   yₛ₋₁::rateType
+  yₛ₋₂::rateType
+  yₛ₋₃::rateType
+  SXₛ₋₁::uType
+  SXₛ₋₂::uType
+  SXₛ₋₃::uType
   Gₛ::noiseRateType
   Xₛ₋₁::noiseRateType
   Xₛ₋₂::noiseRateType
@@ -262,31 +264,32 @@ function alg_cache(alg::KomBurSROCK2,prob,u,ΔW,ΔZ,p,rate_prototype,noise_rate_
 end
 
 function alg_cache(alg::KomBurSROCK2,prob,u,ΔW,ΔZ,p,rate_prototype,noise_rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,f,t,dt,::Type{Val{true}})
-  uᵢ = zero(u)
+  utmp = zero(u)
   uᵢ₋₁ = zero(u)
   uᵢ₋₂ = zero(u)
-  Yₛ₋₁ = zero(u)
-  Yₛ₋₂ = zero(u)
   k = zero(rate_prototype)
-  yₛ₋₁ = zero(rate_prototype)
+  yₛ₋₁ = k
+  yₛ₋₂ = zero(rate_prototype)
+  yₛ₋₃ = zero(rate_prototype)
   Xₛ₋₁ = zero(noise_rate_prototype)
   Xₛ₋₂ = zero(noise_rate_prototype)
   Xₛ₋₃ = zero(noise_rate_prototype)
   vec_χ = false .* vec(ΔW)
-  tmp::uType
-  fsalfirst::rateType
-  atmp::rateType
-  constantcache::KomBurSROCK2ConstantCache
   if typeof(ΔW) <: Number || length(ΔW) == 1 || is_diagonal_noise(prob)
-    uₛ₋₁ = uᵢ
     Gₛ = Xₛ₋₁
+    SXₛ₋₁ = utmp
+    SXₛ₋₂ = utmp
+    SXₛ₋₃ = utmp
   else
-    uₛ₋₁ = zero(u)
     Gₛ = zero(noise_rate_prototype)
+    SXₛ₋₁ = zero(uprev)
+    SXₛ₋₂ = zero(uprev)
+    SXₛ₋₃ = zero(uprev)
   end
   tmp  = uᵢ₋₂            # these 3 variables are dummied to use same memory
   fsalfirst = k
-  atmp = yₛ₋₁
+  atmp = yₛ₋₂
   constantcache = KomBurSROCK2ConstantCache{uEltypeNoUnits}(u)
-  KomBurSROCK2Cache{typeof(u),typeof(k),typeof(noise_rate_prototype),typeof(vec_χ)}(u,uprev,uᵢ,uᵢ₋₁,uᵢ₋₂,uₛ₋₁,Yₛ₋₁,Yₛ₋₂,k,yₛ₋₁,Gₛ,Xₛ₋₁,Xₛ₋₂,Xₛ₋₃,vec_χ,tmp,fsalfirst,atmp,constantcache)
+  KomBurSROCK2Cache{typeof(u),typeof(k),typeof(noise_rate_prototype),typeof(vec_χ)}(u,uprev,utmp,uᵢ₋₁,uᵢ₋₂,k,yₛ₋₁,yₛ₋₂,
+                                            yₛ₋₃,SXₛ₋₁,SXₛ₋₂,SXₛ₋₃,Gₛ,Xₛ₋₁,Xₛ₋₂,Xₛ₋₃,vec_χ,tmp,fsalfirst,atmp,constantcache)
 end
