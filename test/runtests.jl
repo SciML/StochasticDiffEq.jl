53dcc162bc64b1336beb82623886b6e89b235b54
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-23 05:17:14 -0400

    split tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,13 +19,6 @@ const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
     @time @safetestset "tstops Tests" begin include("tstops_tests.jl") end
     @time @safetestset "saveat Tests" begin include("saveat_tests.jl") end
     @time @safetestset "Oval2" begin include("oval2_test.jl") end
-    @time @safetestset "Composite Tests" begin include("composite_algorithm_test.jl") end
-    @time @safetestset "Events Tests" begin include("events_test.jl") end
-    @time @safetestset "Cache Tests" begin include("cache_test.jl") end
-    @time @safetestset "Adaptive Complex Mean Test" begin include("adaptive/sde_complex_adaptive_mean_test.jl") end
-    @time @safetestset "Utility Tests" begin include("utility_tests.jl") end
-    @time @safetestset "Non-diagonal SDE Tests" begin include("nondiagonal_tests.jl") end
-    @time @safetestset "No Index Tests" begin include("noindex_tests.jl") end
   end
 
   if GROUP == "All" || GROUP == "Interface2"
@@ -36,23 +29,36 @@ const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
     @time @safetestset "Scalar Tests" begin include("scalar_noise.jl") end
     @time @safetestset "Stiffness Detection Test" begin include("stiffness_detection_test.jl") end
     @time @safetestset "Adaptive SDE Linear Tests" begin include("adaptive/sde_linearadaptive_tests.jl") end
+  end
+
+  if GROUP == "All" || GROUP == "Interface3"
+    @time @safetestset "Composite Tests" begin include("composite_algorithm_test.jl") end
+    @time @safetestset "Events Tests" begin include("events_test.jl") end
+    @time @safetestset "Cache Tests" begin include("cache_test.jl") end
+    @time @safetestset "Adaptive Complex Mean Test" begin include("adaptive/sde_complex_adaptive_mean_test.jl") end
+    @time @safetestset "Utility Tests" begin include("utility_tests.jl") end
+    @time @safetestset "Non-diagonal SDE Tests" begin include("nondiagonal_tests.jl") end
+    @time @safetestset "No Index Tests" begin include("noindex_tests.jl") end
     @time @safetestset "Multiple Dimension Linear Adaptive Test" begin include("adaptive/sde_twodimlinearadaptive_tests.jl") end
     @time @safetestset "Autostepsize Test" begin include("adaptive/sde_autostepsize_test.jl") end
     @time @safetestset "Additive Lorenz Attractor Test" begin include("adaptive/sde_lorenzattractor_tests.jl") end
   end
 
   if !is_APPVEYOR && (GROUP == "All" || GROUP == "AlgConvergence")
-    @time @safetestset "Rossler Order Tests" begin include("sde/sde_rosslerorder_tests.jl") end
     @time @safetestset "Convergence Tests" begin include("sde/sde_convergence_tests.jl") end
   end
 
   if !is_APPVEYOR && (GROUP == "All" || GROUP == "AlgConvergence2")
-    @time @safetestset "Additive SDE Tests" begin include("sde/sde_additive_tests.jl") end
-    @time @safetestset "Split Tests" begin include("split_tests.jl") end
-    @time @safetestset "Stratonovich Convergence Tests" begin include("stratonovich_convergence_tests.jl") end
     @time @safetestset "IIF Convergence Tests" begin include("iif_methods.jl") end
     LONGER_TESTS && @time @safetestset "Weak Convergence Tests" begin include("weak_convergence.jl") end
     @time @safetestset "Cummutative Noise Methods Tests" begin include("commutative_tests.jl") end
     @time @safetestset "Multivariate Geometric Tests" begin include("multivariate_geometric.jl") end
+  end
+
+  if !is_APPVEYOR && (GROUP == "All" || GROUP == "AlgConvergence3")
+    @time @safetestset "Rossler Order Tests" begin include("sde/sde_rosslerorder_tests.jl") end
+    @time @safetestset "Additive SDE Tests" begin include("sde/sde_additive_tests.jl") end
+    @time @safetestset "Split Tests" begin include("split_tests.jl") end
+    @time @safetestset "Stratonovich Convergence Tests" begin include("stratonovich_convergence_tests.jl") end
   end
 end
