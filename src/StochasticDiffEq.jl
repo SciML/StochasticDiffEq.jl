e1246e68cd2160cef2b4dff582bcfd6e17e0d3f7
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-30 21:14:48 -0400

    add ArrayInterface

--- src/StochasticDiffEq.jl
+++ src/StochasticDiffEq.jl
@@ -6,7 +6,7 @@ module StochasticDiffEq
   @reexport using DiffEqBase
 
   using Parameters, RecursiveArrayTools, DataStructures
-  using DiffEqNoiseProcess, Random
+  using DiffEqNoiseProcess, Random, ArrayInterface
   using NLsolve, ForwardDiff, StaticArrays, MuladdMacro, DiffEqDiffTools
 
   import DiffEqBase: ODE_DEFAULT_NORM, ODE_DEFAULT_ISOUTOFDOMAIN,
