4bc55305efee21407f31c7d8d854dc3823df5c5f
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-02-29 22:22:50 -0500

    no `@everywhere`

--- test/weak_convergence/oop_weak.jl
+++ test/weak_convergence/oop_weak.jl
@@ -1,6 +1,6 @@
-@everywhere using StochasticDiffEq, DiffEqDevTools, Test
-@everywhere using DiffEqProblemLibrary.SDEProblemLibrary: importsdeproblems
-@everywhere importsdeproblems()
+using StochasticDiffEq, DiffEqDevTools, Test
+using DiffEqProblemLibrary.SDEProblemLibrary: importsdeproblems
+importsdeproblems()
 using DiffEqProblemLibrary.SDEProblemLibrary: prob_sde_linear, prob_sde_2Dlinear, prob_sde_additive
 Random.seed!(100)
 dts = 1./2.^(10:-1:2) #14->7 good plot
