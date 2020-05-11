845f4753b6983aa679b59acbaf8f9c93268585bd
Frank Schaefer <frank.schaefer@unibas.ch>
2020-05-11 16:23:31 +0200

    iip for RI1 with tests

--- src/caches/srk_weak_caches.jl
+++ src/caches/srk_weak_caches.jl
@@ -250,3 +250,44 @@ function alg_cache(alg::DRI1,prob,u,ΔW,ΔZ,p,rate_prototype,
 
   DRI1Cache(u,uprev,_dW,_dZ,chi1,Ihat2,tab,g1,g2,g3,k1,k2,k3,H02,H03,H12,H13,H22,H23,tmp1,tmpg)
 end
+
+
+function alg_cache(alg::RI1,prob,u,ΔW,ΔZ,p,rate_prototype,
+                   noise_rate_prototype,uEltypeNoUnits,
+                   uBottomEltypeNoUnits,tTypeNoUnits,uprev,f,t,dt,::Type{Val{true}})
+  if typeof(ΔW) <: Union{SArray,Number}
+    _dW = copy(ΔW)
+    _dZ = copy(ΔW)
+    chi1 = copy(ΔW)
+  else
+    _dW = zero(ΔW)
+    _dZ = zero(ΔW)
+    chi1 = zero(ΔW)
+  end
+  m = length(ΔW)
+  Ihat2 = zeros(eltype(ΔW), m, m)
+  tab = RI1ConstantCache(real(uBottomEltypeNoUnits), real(tTypeNoUnits))
+  g1 = zero(noise_rate_prototype)
+  g2 = [zero(noise_rate_prototype) for k=1:m]
+  g3 = [zero(noise_rate_prototype) for k=1:m]
+  k1 = zero(rate_prototype); k2 = zero(rate_prototype); k3 = zero(rate_prototype)
+
+  H02 = zero(u)
+  H03 = zero(u)
+  H12 = Vector{typeof(u)}()
+  H13 = Vector{typeof(u)}()
+  H22 = Vector{typeof(u)}()
+  H23 = Vector{typeof(u)}()
+
+  for k=1:m
+    push!(H12,zero(u))
+    push!(H13,zero(u))
+    push!(H22,zero(u))
+    push!(H23,zero(u))
+  end
+
+  tmp1 = zero(rate_prototype)
+  tmpg = zero(noise_rate_prototype)
+
+  DRI1Cache(u,uprev,_dW,_dZ,chi1,Ihat2,tab,g1,g2,g3,k1,k2,k3,H02,H03,H12,H13,H22,H23,tmp1,tmpg)
+end
