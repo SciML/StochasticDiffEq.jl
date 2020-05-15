e24b5bde41ed9e128a9ed125bc1399fc75aec3d8
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-15 14:49:20 -0400

    oop tau leaping

--- src/perform_step/tau_leaping.jl
+++ src/perform_step/tau_leaping.jl
@@ -1,6 +1,6 @@
 @muladd function perform_step!(integrator,cache::TauLeapingConstantCache,f=integrator.f)
-  @unpack t,dt,uprev,u,W,p,c = integrator
-  tmp = c(uprev, p, tprev, counts, mark)
+  @unpack t,dt,uprev,u,W,p,P,c = integrator
+  tmp = c(uprev, p, t, P.dW, nothing)
   integrator.u = uprev .+ tmp
 end
 
