3dac15b23e7d43402b1deaf17d86ea0a9fad5cae
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-20 23:43:42 -0400

    speed up a few tests

--- test/weak_convergence/oop_weak.jl
+++ test/weak_convergence/oop_weak.jl
@@ -4,7 +4,7 @@ using DiffEqProblemLibrary.SDEProblemLibrary: importsdeproblems
 importsdeproblems()
 using DiffEqProblemLibrary.SDEProblemLibrary: prob_sde_linear, prob_sde_2Dlinear
 Random.seed!(100)
-dts = 1 .//2 .^(9:-1:2) #14->7 good plot
+dts = 1 .//2 .^(7:-1:3) #14->7 good plot
 
 prob = prob_sde_linear
 println("EM")
