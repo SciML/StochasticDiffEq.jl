d75c93787e666efa42f9ae89e25a6d3ddaa4080d
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-21 08:08:11 -0400

    one last tweak?

--- test/weak_convergence/oop_weak.jl
+++ test/weak_convergence/oop_weak.jl
@@ -57,7 +57,7 @@ sim = test_convergence(dts,prob,SKSROCK(),save_everystep=false,trajectories=Int(
 #@test abs(sim.𝒪est[:weak_l2]-1) < 0.3
 #@test abs(sim.𝒪est[:weak_l∞]-1) < 0.3
 println("SROCKC2")
-dts = 1 .//2 .^(8:-1:2)
+dts = 1 .//2 .^(9:-1:2)
 sim = test_convergence(dts,prob,SROCKC2(),save_everystep=false,trajectories=Int(8e4))
 @test abs(sim.𝒪est[:weak_final]-2) < 0.35
 #@test abs(sim.𝒪est[:weak_l2]-2) < 0.3
