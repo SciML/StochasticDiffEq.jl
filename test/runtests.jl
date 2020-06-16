6c1865fe3ac2b484356c70c938cffa859d527035
Frank Schaefer <frank.schaefer@unibas.ch>
2020-06-16 23:22:19 +0200

    added runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,6 +45,7 @@ const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
     @time @safetestset "Multiple Dimension Linear Adaptive Test" begin include("adaptive/sde_twodimlinearadaptive_tests.jl") end
     @time @safetestset "Autostepsize Test" begin include("adaptive/sde_autostepsize_test.jl") end
     @time @safetestset "Additive Lorenz Attractor Test" begin include("adaptive/sde_lorenzattractor_tests.jl") end
+    @time @safetestset "Weak adaptive Test" begin include("adaptive/sde_weak_adaptive.jl") end
   end
 
   if !is_APPVEYOR && (GROUP == "All" || GROUP == "AlgConvergence")
