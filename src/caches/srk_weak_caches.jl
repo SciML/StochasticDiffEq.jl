3e0fee133903639aa645dc40a35c5e8da3ad42e4
Frank Schaefer <frank.schaefer@unibas.ch>
2020-06-24 23:28:19 +0200

    defined constant cache

--- src/caches/srk_weak_caches.jl
+++ src/caches/srk_weak_caches.jl
@@ -1531,3 +1531,113 @@ function alg_cache(alg::PL1WMA,prob,u,ΔW,ΔZ,p,rate_prototype,
 
   PL1WMACache(u,uprev,_dW,chi1,tab,g1,k1,k2,Y,tmp1)
 end
+
+
+
+#NON
+struct NONConstantCache{T} <: StochasticDiffEqConstantCache
+  c01::T
+  c02::T
+  c03::T
+  c04::T
+
+  cj1::T
+  cj2::T
+  cj3::T
+  cj4::T
+
+  cjl2::T
+  cjl3::T
+
+  clj2::T
+  clj3::T
+
+  a0021::T
+  a0032::T
+  a0043::T
+
+  aj021::T
+  aj041::T
+
+  a0j21::T
+  a0j31::T
+  a0j32::T
+  a0j41::T
+
+  ajj21::T
+  ajj31::T
+  ajj32::T
+  ajj41::T
+  ajj42::T
+  ajj43::T
+
+  ajl31::T
+  ajl32::T
+  ajl41::T
+  ajl42::T
+
+  ajljj31::T
+
+  aljjl21::T
+  ajljj31::T
+
+  #quantile(Normal(),1/6)
+  NORMAL_ONESIX_QUANTILE::T
+end
+
+
+function NONConstantCache(T::Type)
+  c01 = convert(T, 1//6)
+  c02 = convert(T, 1//3)
+  c03 = convert(T, 1//3)
+  c04 = convert(T, 1//6)
+
+  cj1 = convert(T, 1//8)
+  cj2 = convert(T, 3//8)
+  cj3 = convert(T, 3//8)
+  cj4 = convert(T, 1//8)
+
+  cjl2 = convert(T, 1//2)
+  cjl3 = convert(T, -1//2)
+
+  clj2 = convert(T, -1//2)
+  clj3 = convert(T, 1//2)
+
+  a0021 = convert(T, 1//2)
+  a0032 = convert(T, 1//2)
+  a0043 = convert(T, 1)
+
+  aj021 = convert(T, 2)
+  aj041 = convert(T, -2)
+
+  a0j21 = convert(T, 1)
+  a0j31 = convert(T, -9//8)
+  a0j32 = convert(T, 9//8)
+  a0j41 = convert(T, 1)
+
+  ajj21 = convert(T, 2//3)
+  ajj31 = convert(T, 1//12)
+  ajj32 = convert(T, 1//4)
+  ajj41 = convert(T, -5//4)
+  ajj42 = convert(T, 1//4)
+  ajj43 = convert(T, 2)
+
+  ajl31 = convert(T, 1//4)
+  ajl32 = convert(T, 3//4)
+  ajl41 = convert(T, 1//4)
+  ajl42 = convert(T, 3//4)
+
+  ajljj31 = convert(T, 1)
+
+  aljjl21 = convert(T, -1//2)
+  ajljj31 = convert(T, 1//2)
+
+  NORMAL_ONESIX_QUANTILE = convert(T,-0.9674215661017014)
+
+  NONConstantCache(c01,c02,c03,c04,cj1,cj2,cj3,cj4,cjl2,cjl3,clj2,clj3,a0021,a0032,a0043,aj021,aj041,a0j21,a0j31,a0j32,a0j41,ajj21,ajj31,ajj32,ajj41,ajj42,ajj43,ajl31,ajl32,ajl41,ajl42,ajljj31,aljjl21,ajljj31,NORMAL_ONESIX_QUANTILE)
+end
+
+
+function alg_cache(alg::NON,prob,u,ΔW,ΔZ,p,rate_prototype,noise_rate_prototype,jump_rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,f,t,dt,::Type{Val{false}})
+  NONConstantCache(real(uBottomEltypeNoUnits))
+end
