4fd706a1279c1a0d7a2d3577e289ee9e5d0196b4
Frank Schaefer <frank.schaefer@unibas.ch>
2021-01-11 21:56:51 +0100

    add local (test error looks random)

--- test/sde/sde_twodimlinear_tests.jl
+++ test/sde/sde_twodimlinear_tests.jl
@@ -109,6 +109,7 @@ print(".")
 
 eigen_est = (integrator) -> integrator.eigen_est = 10.0
 for Alg in [SROCK1, SROCK2], alg in [Alg(), Alg(eigen_est=eigen_est)]
+  local sim2
   sim2 = test_convergence(dts,prob,alg,trajectories=100)
   @test abs(sim2.𝒪est[:l∞]-1) < 0.2
 end
