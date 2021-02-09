3d0e1b2e5c07c0aa4189656977eeb7fcd26178a9
James Gardner <james.gardner1421@gmail.com>
2021-02-09 11:42:24 +0000

    Add convergence tests

--- test/sde/sde_dynamical.jl
+++ test/sde/sde_dynamical.jl
@@ -1,22 +1,31 @@
-using StochasticDiffEq, DiffEqNoiseProcess, Test, DiffEqDevTools
+using StochasticDiffEq, DiffEqNoiseProcess, Test, DiffEqDevTools, Random
+Random.seed!(1)
 
 u0 = zeros(2)
 v0 = ones(2)
-γ = 1
+γ = 50
+
 f1_harmonic(v,u,p,t) = -u
 f2_harmonic(v,u,p,t) = v
-g(u,p,t) = 0.2
-
-ff_harmonic = DynamicalSDEFunction(f1_harmonic,f2_harmonic,g)
-prob = DynamicalSDEProblem(ff_harmonic,g,v0,u0,(0.0,5.0))
-
-sol1 = solve(prob,BAOAB(gamma=γ);dt=1/10,save_noise=true)
+g(u,p,t) = sqrt(2γ)
 
 f1_harmonic_iip(dv,v,u,p,t) = dv .= f1_harmonic(v,u,p,t)
 f2_harmonic_iip(du,v,u,p,t) = du .= f2_harmonic(v,u,p,t)
 g_iip(du,u,p,t) = du .= g(u,p,t)
 
-prob = DynamicalSDEProblem(f1_harmonic_iip,f2_harmonic_iip,g_iip,v0,u0,(0.0,5.0); noise=NoiseWrapper(sol1.W))
+ff_harmonic = DynamicalSDEFunction(f1_harmonic,f2_harmonic,g)
+prob1 = DynamicalSDEProblem(ff_harmonic,g,v0,u0,(0.0,5.0))
+sol1 = solve(prob1,BAOAB(gamma=γ);dt=1/10,save_noise=true)
 
-sol2 = solve(prob,BAOAB(gamma=γ);dt=1/10)
+prob2 = DynamicalSDEProblem(f1_harmonic_iip,f2_harmonic_iip,g_iip,v0,u0,(0.0,5.0); noise=NoiseWrapper(sol1.W))
+sol2 = solve(prob2,BAOAB(gamma=γ);dt=1/10)
+
 @test sol1[:] ≈ sol2[:]
+
+dts = (1/2) .^ (9:-1:3)
+# Can't use NoiseGrid as noise is not generated with the correct size in convergence.jl. We require noise with shape of v.
+# I don't think there are any analytic solutions for problems of this type
+sim1  = analyticless_test_convergence(dts,prob1,BAOAB(gamma=γ),(1/2)^10;trajectories=Int(1e2),use_noise_grid=false)
+@test abs(sim1.𝒪est[:weak_final]-1) < 0.3 # Gives order of only 1? Should be weak order 2.
+sim2  = analyticless_test_convergence(dts,prob2,BAOAB(gamma=γ),(1/2)^10;trajectories=Int(1e2),use_noise_grid=false)
+@test abs(sim2.𝒪est[:weak_final]-1) < 0.3 # Gives order of only 1? Should be weak order 2.
