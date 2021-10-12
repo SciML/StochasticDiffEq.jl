ae387f32769811c98328633b8c115d13128a3601
Frank Schaefer <frank.schaefer@unibas.ch>
2021-10-12 22:43:49 +0200

    Update weak_strat_non_diagonal.jl

--- test/weak_convergence/weak_strat_non_diagonal.jl
+++ test/weak_convergence/weak_strat_non_diagonal.jl
@@ -13,7 +13,7 @@ using Random
 using DiffEqDevTools
 #using DiffEqGPU
 
-seed = 10
+seed = 1234
 
 function prob_func(prob, i, repeat)
     remake(prob,seed=seeds[i])
@@ -47,7 +47,7 @@ ensemble_prob = EnsembleProblem(prob;
         prob_func = prob_func
         )
 
-numtraj = Int(2e7)
+numtraj = Int(1e7)
 Random.seed!(seed)
 seeds = rand(UInt, numtraj)
 
