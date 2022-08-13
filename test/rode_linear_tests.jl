7c1a353e5582d1a6b86f5b53c4ae3cd6157b5e1b
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-08-13 10:27:33 -0400

    Fix old RODE call

--- test/rode_linear_tests.jl
+++ test/rode_linear_tests.jl
@@ -6,7 +6,7 @@ tspan = (0.0,1.0)
 prob = RODEProblem(f,u0,tspan)
 sol = solve(prob,RandomEM(),dt=1/100)
 
-f(u,p,t,W,du) = (du.=1.01u.+0.87u.*W)
+f(du,u,p,t,W) = (du.=1.01u.+0.87u.*W)
 u0 = ones(4)
 prob = RODEProblem(f,u0,tspan)
 sol = solve(prob,RandomEM(),dt=1/100)
