62520490f9dc550eb23585a7e342144912d2ad1a
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-12-26 10:21:13 -0500

    fix

--- test/complex_tests.jl
+++ test/complex_tests.jl
@@ -39,14 +39,6 @@ end
 
   # currently broken
   for alg in implicit_autodiff
-    @test_throws OrdinaryDiffEq.FirstAutodiffJacError solve(prob, alg)
+    @test_throws ArgumentError solve(prob, alg)
   end
 end
-
-u0 = ones(2,4) + im*ones(2,4)
-function f(du, u, p, t)
-  t isa Complex && error("time is complex")
-  du .= 1.01u
-end
-prob = SDEProblem(f, f, u0, tspan)
-solve(prob, SKenCarp())
