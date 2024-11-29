0e4ac2328f572fbb1a47bb8adfbc93dd2537e2bc
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2024-11-29 13:30:54 +0530

    refactor: relax `initialize_dae!` type restrictions

--- src/initialize_dae.jl
+++ src/initialize_dae.jl
@@ -1,13 +1,13 @@
 struct SDEDefaultInit <: DiffEqBase.DAEInitializationAlgorithm end
 
-function DiffEqBase.initialize_dae!(integrator::SDEIntegrator, initializealg = integrator.initializealg)
+function DiffEqBase.initialize_dae!(integrator::AbstractSDEIntegrator, initializealg = integrator.initializealg)
     OrdinaryDiffEqCore._initialize_dae!(integrator, integrator.sol.prob, initializealg, Val(DiffEqBase.isinplace(integrator.sol.prob)))
 end
 
-function OrdinaryDiffEqCore._initialize_dae!(integrator::SDEIntegrator, prob, ::SDEDefaultInit, isinplace)
+function OrdinaryDiffEqCore._initialize_dae!(integrator::AbstractSDEIntegrator, prob, ::SDEDefaultInit, isinplace)
     if SciMLBase.has_initializeprob(prob.f)
         OrdinaryDiffEqCore._initialize_dae!(integrator, prob, SciMLBase.OverrideInit(), isinplace)
-    else
+    elseif SciMLBase.__has_mass_matrix(prob.f)
         OrdinaryDiffEqCore._initialize_dae!(integrator, prob, SciMLBase.CheckInit(), isinplace)
     end
 end
