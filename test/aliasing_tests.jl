cda7a3ad383d1dcfeea08024a6ad3668168c045f
jClugstor <jadonclugston@gmail.com>
2025-01-16 17:42:38 -0500

    add more tests

--- test/aliasing_tests.jl
+++ test/aliasing_tests.jl
@@ -6,6 +6,8 @@ using SciMLBase
 # Test that the old keyword works, and that the new AliasSpecier works.
 @test_warn x -> contains("`alias_u0`", x) solve(prob_sde_linear, EM(), dt=0.1, alias_u0=true)
 
+@test_nowarn solve(prob_sde_linear, EM(), dt=0.1)
+
 @test_nowarn solve(prob_sde_linear, EM(), dt = 0.1, alias = SciMLBase.SDEAliasSpecifier(alias_u0 = true))
 
 function f3(u, p, t, W)
@@ -13,8 +15,10 @@ function f3(u, p, t, W)
 end
 u0 = 1.00
 tspan = (0.0, 5.0)
-prob = RODEProblem(f3, u0, tspan)
+prob_rode = RODEProblem(f3, u0, tspan)
 
-@test_warn x -> contains("`alias_u0`", x) sol = solve(prob, RandomEM(), dt=1 / 100)
+@test_warn x -> contains("`alias_u0`", x) sol = solve(prob_rode, RandomEM(), dt=1 / 100, alias_u0 = true)
 
-@test_nowarn solve(prob_sde_linear, EM(), dt=0.1, alias=SciMLBase.RODEAliasSpecifier(alias_u0=true))
+@test_nowarn solve(prob_rode, RandomEM(), dt=0.1)
+
+@test_nowarn solve(prob_rode, RandomEM(), dt=0.1, alias=SciMLBase.RODEAliasSpecifier(alias_u0=true))
