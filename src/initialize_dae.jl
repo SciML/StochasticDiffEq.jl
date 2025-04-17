d62b7f4238e032f20f3e4fc4dc318ffd279dc6db
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-04-17 16:55:37 -0400

    Update src/initialize_dae.jl

--- src/initialize_dae.jl
+++ src/initialize_dae.jl
@@ -2,7 +2,7 @@ function DiffEqBase.initialize_dae!(integrator::Union{AbstractSDEIntegrator, Abs
     OrdinaryDiffEqCore._initialize_dae!(integrator, integrator.sol.prob, initializealg, Val(DiffEqBase.isinplace(integrator.sol.prob)))
 end
 
-function OrdinaryDiffEqCore._initialize_dae!(integrator::Union{AbstractSDEIntegrator, AbstractSDDEIntegrator}, prob::AbstractSDEProblem, ::OrdinaryDiffEqCore.DefaultInit, isinplace)
+function OrdinaryDiffEqCore._initialize_dae!(integrator::Union{AbstractSDEIntegrator, AbstractSDDEIntegrator}, prob::SciMLBase.AbstractSDEProblem, ::OrdinaryDiffEqCore.DefaultInit, isinplace)
     if SciMLBase.has_initializeprob(prob.f)
         OrdinaryDiffEqCore._initialize_dae!(integrator, prob, SciMLBase.OverrideInit(), isinplace)
     elseif SciMLBase.__has_mass_matrix(prob.f)
