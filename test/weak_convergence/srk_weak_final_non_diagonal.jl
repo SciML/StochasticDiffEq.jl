6deb5cc15caf12625e031be07bd5dff2ae043754
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-10-23 08:32:46 -0400

    Update srk_weak_final_non_diagonal.jl

--- test/weak_convergence/srk_weak_final_non_diagonal.jl
+++ test/weak_convergence/srk_weak_final_non_diagonal.jl
@@ -114,7 +114,8 @@ sim = test_convergence(dts, ensemble_prob, RDI1WM(),
     weak_timeseries_errors = false, weak_dense_errors = false,
     expected_value = exp(-3.0)
 )
-@test_broken abs(sim.𝒪est[:weak_final]-1.5) < 0.3 # seems closer to 1.5?
+@test_broken abs(sim.𝒪est[:weak_final]-2.0) < 0.3 # seems closer to 1.5?
+@test abs(sim.𝒪est[:weak_final]-1.5) < 0.3 # seems closer to 1.5?
 println("RDI1WM:", sim.𝒪est[:weak_final])
 
 numtraj = Int(1e7)
@@ -127,7 +128,7 @@ sim = test_convergence(dts, ensemble_prob, RDI2WM(),
     weak_timeseries_errors = false, weak_dense_errors = false,
     expected_value = exp(-3.0)
 )
-@test -(sim.𝒪est[:weak_final]-2.5) < 0.3 # order 2.517769274990593
+@test abs(sim.𝒪est[:weak_final]-2.0) < 0.3
 println("RDI2WM:", sim.𝒪est[:weak_final])
 
 sim = test_convergence(dts, ensemble_prob, RDI3WM(),
@@ -135,7 +136,7 @@ sim = test_convergence(dts, ensemble_prob, RDI3WM(),
     weak_timeseries_errors = false, weak_dense_errors = false,
     expected_value = exp(-3.0)
 )
-@test abs(sim.𝒪est[:weak_final]-2) < 0.3
+@test abs(sim.𝒪est[:weak_final]-3) < 0.4
 println("RDI3WM:", sim.𝒪est[:weak_final])
 
 sim = test_convergence(dts, ensemble_prob, RDI4WM(),
@@ -143,5 +144,5 @@ sim = test_convergence(dts, ensemble_prob, RDI4WM(),
     weak_timeseries_errors = false, weak_dense_errors = false,
     expected_value = exp(-3.0)
 )
-@test abs(sim.𝒪est[:weak_final]-2) < 0.3
+@test abs(sim.𝒪est[:weak_final]-3) < 0.4
 println("RDI4WM:", sim.𝒪est[:weak_final])
