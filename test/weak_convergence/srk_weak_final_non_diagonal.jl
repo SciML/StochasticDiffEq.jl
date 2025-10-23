567688fc9bfdc708c69d5bc7788f6d0105f22c1b
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-10-23 07:23:13 -0400

    Update test/weak_convergence/srk_weak_final_non_diagonal.jl

--- test/weak_convergence/srk_weak_final_non_diagonal.jl
+++ test/weak_convergence/srk_weak_final_non_diagonal.jl
@@ -113,7 +113,7 @@ sim = test_convergence(dts, ensemble_prob, RDI1WM(),
     save_everystep = false, trajectories = numtraj, save_start = false, adaptive = false,
     weak_timeseries_errors = false, weak_dense_errors = false,
     expected_value = exp(-3.0)
-
+)
 @test_broken abs(sim.𝒪est[:weak_final]-1.5) < 0.3 # seems closer to 1.5?
 println("RDI1WM:", sim.𝒪est[:weak_final])
 
